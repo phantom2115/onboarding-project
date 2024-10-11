79746eb266ecf4cebdeaba733012a2578b1bb7a4
minseok <minskyson7@gmail.com>
2024-10-11 23:25:27 +0900

    chore: add axios

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,7 +1,9 @@
-const getPosts = async () => {
-  return await fetch("https://jsonplaceholder.typicode.com/posts").then(
-    (response) => response.json()
-  );
-};
+// const getPosts = async () => {
+//   return await fetch("https://jsonplaceholder.typicode.com/posts").then(
+//     (response) => response.json()
+//   );
+// };
+
+
 
 export { getPosts };
